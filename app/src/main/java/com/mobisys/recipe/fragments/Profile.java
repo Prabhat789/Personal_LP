70776a5a51898c4d3eec477b552d07d01d50039f
Prabhat789 <prabhat.kumar.ec@gmail.com>
2016-03-16 19:14:55 +0530

    added profile screen

--- app/src/main/java/com/mobisys/recipe/fragments/Profile.java
+++ app/src/main/java/com/mobisys/recipe/fragments/Profile.java
@@ -1,9 +1,23 @@
 package com.mobisys.recipe.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mobisys.recipe.R;
 
 /**
  * Created by ubuntu1 on 11/3/16.
  */
 public class Profile extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return rootView;
+    }
 }
