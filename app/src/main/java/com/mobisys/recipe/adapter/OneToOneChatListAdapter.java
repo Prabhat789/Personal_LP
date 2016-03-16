8ea82853a6a520e63c1c3d0f09891660b1b2bd76
Prabhat789 <prabhat.kumar.ec@gmail.com>
2016-03-16 11:57:34 +0530

    login_preference added

--- app/src/main/java/com/mobisys/recipe/adapter/OneToOneChatListAdapter.java
+++ app/src/main/java/com/mobisys/recipe/adapter/OneToOneChatListAdapter.java
@@ -70,52 +70,38 @@ public class OneToOneChatListAdapter extends RecyclerView.Adapter<OneToOneChatLi
     @Override
     public void onBindViewHolder(DataObjectHolder holder, int position) {
         final boolean isMe = mDataset.get(position).getUserId().equals(mUserId);
-       // LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         if (isMe) {
             holder.imageRight.setVisibility(View.VISIBLE);
             holder.imageLeft.setVisibility(View.GONE);
-           // holder.txtBody.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
-           // params.gravity = Gravity.CENTER_VERTICAL | Gravity.RIGHT;
-             holder.txtBody.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
-            //holder.txtBody.setLayoutParams(params);
+
+            //holder.txtBody.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
             holder.txtBody.setTextColor(mContext.getResources().getColor(R.color.smoke_white));
             holder.llBody.setGravity(Gravity.CENTER_VERTICAL |Gravity.RIGHT);
-           // holder.txtUserName.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
-           // holder.llBody.setBackground(mContext.getResources().getDrawable(R.drawable.bg_me));
-            // holder.body.setTextColor(Color.parseColor("#29464A"));
 
         } else {
             holder.imageLeft.setVisibility(View.VISIBLE);
             holder.imageRight.setVisibility(View.GONE);
 
-           // params.gravity = Gravity.CENTER_VERTICAL | Gravity.LEFT;
             holder.txtBody.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
-           // holder.txtBody.setLayoutParams(params);
             holder.txtBody.setTextColor(mContext.getResources().getColor(R.color.cardview_dark_background));
             holder.llBody.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
-           // holder.txtUserName.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
-            //holder.llBody.setBackground(mContext.getResources().getDrawable(R.drawable.bg_friend));
-            //  holder.body.setTextColor(Color.parseColor("#301041"));
+
 
         }
         final NetworkImageView profileView = (NetworkImageView) (isMe ? holder.imageRight : holder.imageLeft);
         try {
             if (mDataset.get(position).getProfileImage().length() != 0) {
-                /*imageLoader.displayImage(message.getProfileImage(),
-                        profileView, options);*/
+
                 imageLoader.get( mDataset.get(position).getProfileImage(), ImageLoader.getImageListener(
                         profileView,
                         R.mipmap.ic_launcher, R.mipmap.ic_launcher));
                 profileView.setImageUrl( mDataset.get(position).getProfileImage(), imageLoader);
-                //Picasso.with(getContext()).load(message.getProfileImage()).into(profileView);
             }else {
-                /*imageLoader.displayImage(getProfileUrl(message.getUserId()),
-                        profileView, options);*/
+
                 imageLoader.get(getProfileUrl( mDataset.get(position).getUserId()), ImageLoader.getImageListener(
                         profileView,
                         R.mipmap.ic_launcher, R.mipmap.ic_launcher));
                 profileView.setImageUrl(mDataset.get(position).getProfileImage(), imageLoader);
-                //Picasso.with(getContext()).load(getProfileUrl(message.getUserId())).into(profileView);
             }
         } catch (Exception e) {
             // TODO: handle exception
@@ -123,7 +109,6 @@ public class OneToOneChatListAdapter extends RecyclerView.Adapter<OneToOneChatLi
         }
 
         holder.txtBody.setText(mDataset.get(position).getBody());
-        //holder.body.setTypeface(myTypeface);
         //holder.txtUserName.setText(mDataset.get(position).getUserName() + " at " + mDataset.get(position).getDateTime());
         profileView.setOnClickListener(new View.OnClickListener() {
 
