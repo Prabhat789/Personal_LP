39f9e11831ecafee643e0adba0ef8ad0cc69bc51
Prabhat789 <prabhat.kumar.ec@gmail.com>
2016-03-16 11:40:51 +0530

    login_preference added

--- app/src/main/java/com/mobisys/recipe/util/ApplicationConstant.java
+++ app/src/main/java/com/mobisys/recipe/util/ApplicationConstant.java
@@ -8,5 +8,9 @@ public class ApplicationConstant {
     public static final String TEXT_FONT_NAME = "alighty.ttf";
     public static final String BUTTON_FONT_NAME = "alighty.ttf";
     public static final String EDIT_TEXT_FONT_NAME = "alighty.ttf";
+    public static final String LOGIN_PREFERENCE = "loginPrefs";
+    public static final String SAVE_LOGIN = "saveLogin";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
 
 }
