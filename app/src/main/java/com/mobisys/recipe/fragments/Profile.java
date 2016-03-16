f88d70d2f6c235f006a825572e4691a73ac91fdf
Prabhat <prabhat.kumar.ec@gmail.com>
2016-03-17 03:56:10 +0530

    profile screen changes

--- app/src/main/java/com/mobisys/recipe/fragments/Profile.java
+++ app/src/main/java/com/mobisys/recipe/fragments/Profile.java
@@ -6,18 +6,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.mobisys.recipe.R;
+import com.mobisys.recipe.util.CustomVolleyRequestQueue;
+import com.parse.ParseUser;
 
 /**
  * Created by ubuntu1 on 11/3/16.
  */
 public class Profile extends Fragment {
 
+    private static final String TAG = Profile.class.getSimpleName();
+    private NetworkImageView userProfileImage;
+    private ImageLoader imageLoader;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        userProfileImage = (NetworkImageView)rootView.findViewById(R.id.userProfileImage);
+        imageLoader = CustomVolleyRequestQueue.getInstance(getActivity()).getImageLoader();
+        /*imageLoader.get(ParseUser.getCurrentUser().getString("profileImage"), ImageLoader.getImageListener(
+                userProfileImage,
+                R.mipmap.ic_launcher, R.mipmap.ic_launcher));*/
+        userProfileImage.setImageUrl(ParseUser.getCurrentUser().getString("profileImage"), imageLoader);
+
 
         return rootView;
     }
+
+
 }
